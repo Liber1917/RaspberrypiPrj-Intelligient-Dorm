f447319d817d32038df3c591ff89644e1601f5d0
2741184145 <149482783+2741184145@users.noreply.github.com>
2023-11-21 18:36:57 +0800

    bluetooth nimble demo

--- main.lua
+++ main.lua
@@ -1,87 +1,66 @@
 
 -- LuaTools需要PROJECT和VERSION这两个信息
-PROJECT = "gpiodemo"
-VERSION = "1.0.1"
+PROJECT = "bledemo"
+VERSION = "1.0.0"
+
+--[[
+BLE peripheral的demo, 等待被连接的设备
+支持的模块:
+1. Air101/Air103, 开发板的BLE天线未引出, 需要靠近使用, 且功耗高
+2. ESP32系列, 包括ESP32C3/ESP32S3
+
+-- 配合微信小程序 "LuatOS蓝牙调试"
+-- 1. 若开发板无天线, 将手机尽量靠近芯片也能搜到
+-- 2. 该小程序是开源的, 每次write会自动分包
+-- https://gitee.com/openLuat/luatos-miniapps
+]]
 
 log.info("main", PROJECT, VERSION)
 
--- sys库是标配
-_G.sys = require("sys")
+-- 一定要添加sys.lua !!!!
+sys = require("sys")
 
+--添加硬狗防止程序卡死
 if wdt then
-    --添加硬狗防止程序卡死，在支持的设备上启用这个功能
     wdt.init(9000)--初始化watchdog设置为9s
     sys.timerLoopStart(wdt.feed, 3000)--3s喂一次狗
 end
 
--- Air780E的AT固件默认会为开机键防抖, 导致部分用户刷机很麻烦
-if rtos.bsp() == "EC618" and pm and pm.PWK_MODE then
-    pm.power(pm.PWK_MODE, false)
-end
-
---下面的GPIO引脚编号，请根据实际需要进行更改！
--- Air101开发板的3个LED分别为 PB08/PB09/PB10
--- Air103开发板的3个LED分别为 PB24/PB25/PB26
--- Air105开发板的3个LED分别为 PD14/PD15/PC3
-
--- 若下载到设备后提示pin库不存在,请升级固件到V0006或以上
-
-
---【HaoSir2022】于2022年4月21日增加
-local rtos_bsp = rtos.bsp()
-function pinx() -- 根据不同开发板，给LED赋值不同的gpio引脚编号
-    if rtos_bsp == "AIR101" then -- Air101开发板LED引脚编号
-        return pin.PB08, pin.PB09, pin.PB10
-    elseif rtos_bsp == "AIR103" then -- Air103开发板LED引脚编号
-        return pin.PB26, pin.PB25, pin.PB24
-    elseif rtos_bsp == "AIR601" then -- Air103开发板LED引脚编号
-        return pin.PA7, 255, 255
-    elseif rtos_bsp == "AIR105" then -- Air105开发板LED引脚编号
-        return pin.PD14, pin.PD15, pin.PC3
-    elseif rtos_bsp == "ESP32C3" then -- ESP32C3开发板的引脚
-        return 12, 13, 255 -- 开发板上就2个灯
-    elseif rtos_bsp == "ESP32S3" then -- ESP32C3开发板的引脚
-        return 10, 11, 255 -- 开发板上就2个灯
-    elseif rtos_bsp == "EC618" then -- Air780E开发板引脚
-        return 27, 255, 255 -- AIR780E开发板上就一个灯
-    elseif rtos_bsp == "UIS8850BM" then -- Air780UM开发板引脚
-        return 36, 255, 255 -- Air780UM开发板上就一个灯
-    else
-        log.info("main", "define led pin in main.lua")
-        return 0, 0, 0
-    end
-end
-
-
---LED引脚判断赋值结束
-
-local P1,P2,P3=pinx()--赋值开发板LED引脚编号
-local LEDA= gpio.setup(P1, 0, gpio.PULLUP)
-local LEDB= gpio.setup(P2, 0, gpio.PULLUP)
-local LEDC= gpio.setup(P3, 0, gpio.PULLUP)
-
-
-sys.taskInit(function()
---开始流水灯
-    local count = 0
-    while 1 do
-    --流水灯程序
-        sys.wait(500) --点亮时间
-        -- 轮流点灯
-        LEDA(count % 3 == 0 and 1 or 0)
-        if P2 and P2 ~=255 then
-            LEDB(count % 3 == 1 and 1 or 0)
-        end
-        if P3 and P3 ~= 255 then
-            LEDC(count % 3 == 2 and 1 or 0)
-        end
-        log.info("GPIO", "Go Go Go", count, rtos.bsp())
-        log.info("LuatOS:", "https://wiki.luatos.com")
-        count = count + 1
-    end
+-- 监听GATT服务器的WRITE_CHR, 也就是收取数据的回调
+sys.subscribe("BLE_GATT_WRITE_CHR", function(info, data)
+    -- info 是个table, 但当前没有数据
+    log.info("ble", "data got!!", data:toHex())
 end)
 
--- API文档 https://wiki.luatos.com/api/gpio.html
+sys.taskInit(function()
+    sys.wait(2000)
+
+    -- BLE模式, 默认是SERVER/Peripheral,即外设模式, 等待被连接的设
+    -- nimble.mode(nimble.MODE_BLE_SERVER) -- 默认就是它, 不用调用
+
+    -- 设置SERVER/Peripheral模式下的UUID, 支持设置3个
+    -- 地址支持 2/4/16字节, 需要二进制数据, 例如 string.fromHex("AABB") 返回的是2个字节数据,0xAABB
+    if nimble.setUUID then -- 2023-02-25之后编译的固件支持本API
+        nimble.setUUID("srv", string.fromHex("380D"))      -- 服务主UUID         ,  默认值 180D
+        nimble.setUUID("write", string.fromHex("FF31"))    -- 往本设备写数据的UUID,  默认值 FFF1
+        nimble.setUUID("indicate", string.fromHex("FF32")) -- 订阅本设备的数据的UUID,默认值 FFF2
+    end
+
+    -- 可以自定义名称
+    -- nimble.init("LuatOS-Wendal") -- 蓝牙名称可修改,也有默认值LOS-$mac地址
+    nimble.init() -- 蓝牙名称可修改,也有默认值LOS-$mac地址
+
+    sys.wait(500)
+    -- 打印MAC地址
+    local mac = nimble.mac()
+    log.info("ble", "mac", mac and mac:toHex() or "Unknwn")
+
+    -- 发送数据
+    while 1 do
+        sys.wait(3000)
+        nimble.send_msg(1, 0, string.char(0x5A, 0xA5, 0x12, 0x34, 0x56))
+    end
+end)
 
 -- 用户代码已结束---------------------------------------------
 -- 结尾总是这一句
