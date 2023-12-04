5f4a6332960271d829549659307e0da9ea589cf9
yyc <y18640434857@163.com>
2023-12-04 18:51:02 +0800

    语音激活成功
    snowboy只能在linux、树梅派上运行

--- Intel_Dorm_main/main.py
+++ Intel_Dorm_main/main.py
@@ -3,6 +3,21 @@ from queue import Queue
 from voice.record_10s import record_every_10s
 from voice.voice_vosk import voice_recognization
 from order_request.order_request import order_react
+from snowboy import snowboydecoder_2
+import signal
+
+interrupted = False
+
+
+def signal_handler(signal, frame):
+    global interrupted
+    interrupted = True
+
+
+def interrupt_callback():
+    global interrupted
+    return interrupted
+
 
 order_queue = Queue()
 
@@ -11,11 +26,28 @@ thread2 = threading.Thread(target=voice_recognization, args=(order_queue,))
 thread3 = threading.Thread(target=order_react, args=(order_queue,))
 
 if __name__ == '__main__':
-    # 启动线程
-    thread.start()
-    thread2.start()
-    thread3.start()
+    model = "../snowboy/wake_up.pmdl"
 
-    thread.join()
-    thread2.join()
-    thread3.join()
+    # capture SIGINT signal, e.g., Ctrl+C
+    signal.signal(signal.SIGINT, signal_handler)
+
+    detector = snowboydecoder_2.HotwordDetector(model, sensitivity=0.5)
+    print('Listening... Press Ctrl+C to exit')
+
+    # main loop
+    right = detector.start(detected_callback=snowboydecoder_2.play_audio_file(),
+                           interrupt_check=interrupt_callback,
+                           sleep_time=0.03)
+    print(right)
+
+    detector.terminate()
+    # 启动线程
+    if right:
+        print("start")
+        thread.start()
+        thread2.start()
+        thread3.start()
+
+        thread.join()
+        thread2.join()
+        thread3.join()
