b742d2934be830041e95670c81387bb57e45e7a2
yyc <y18640434857@163.com>
2023-12-05 17:39:41 +0800

    仅作上课用

--- snowboy/snowboy_start.py
+++ snowboy/snowboy_start.py
@@ -1,5 +1,6 @@
 import snowboydecoder_2
-import sys
+import wave
+import pyaudio
 import signal
 
 interrupted = False
@@ -15,6 +16,22 @@ def interrupt_callback():
     return interrupted
 
 
+def play_wav_file(filename):
+    wav_file = wave.open(filename, 'rb')
+    audio_stream = pyaudio.PyAudio().open(format=pyaudio.get_format_from_width(wav_file.getsampwidth()),
+                                          channels=wav_file.getnchannels(),
+                                          rate=wav_file.getframerate(),
+                                          output=True)
+    chunk_size = 1024
+    data = wav_file.readframes(chunk_size)
+    while data:
+        audio_stream.write(data)
+        data = wav_file.readframes(chunk_size)
+    audio_stream.stop_stream()
+    audio_stream.close()
+    wav_file.close()
+
+
 model = "../snowboy/wake_up.pmdl"
 
 # capture SIGINT signal, e.g., Ctrl+C
@@ -28,5 +45,7 @@ right = detector.start(detected_callback=snowboydecoder_2.play_audio_file(),
                interrupt_check=interrupt_callback,
                sleep_time=0.03)
 print(right)
+if right:
+    play_wav_file('../科莱让你别卷了.wav')
 
 detector.terminate()
